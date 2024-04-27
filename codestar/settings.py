72f724d705138a9879f9fb0e5a8a1d5fa538d602
DavidTausend <davidtausend-911@hotmail.com>
2024-04-27 12:23:55 +0000

    add post

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'os.environ.get("SECRET_KEY")'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-davidtausend-djangoblog-25ofx05avdm.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-davidtausend-djangoblog-l8crrnhfn0y.ws-eu110.gitpod.io']
 
 
 # Application definition
