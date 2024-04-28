aa810fbfb950b3d78e2a0fa73366599e1faaf56e
DavidTausend <davidtausend-911@hotmail.com>
2024-04-28 13:17:05 +0000

    enable serving of static files

--- eny.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgres://pokuzwtk:PLRTScac57n6BBTqu2YTTTVy16LH6cjd@cornelius.db.elephantsql.com/pokuzwtk")
-os.environ.setdefault("SECRET_KEY", "pbkdf2_sha256$600000$Hb8SITFVv9fqjj8RtZcWAj$VL7HwkUz8UA+uG/bRTr/6EBa9vsOOkHcYbpzbYril+M=")
